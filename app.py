43c322f5eeac15c7ed59a807edbb930f512de082
Valdas <57098027+valdas-v1@users.noreply.github.com>
2021-02-01 14:42:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ import pandas as pd
 from database import Database
 
 SAVED_MODEL_PATH = "model_files/house_price_predictor.pkl"
+SAVED_LABEL_ENCODER_PATH = "model_files/label_encoder.pkl"
 
-# Loading the classifier from file
+# Loading the classifier and label encoder from file
 classifier = pickle.load(open(SAVED_MODEL_PATH, "rb"))
+le = pickle.load(open(SAVED_LABEL_ENCODER_PATH, "rb"))
 
 app = Flask(__name__)
 db = Database()
+
 
 def __process_input(request_data: list) -> pd.DataFrame:
     """
@@ -20,10 +23,52 @@ def __process_input(request_data: list) -> pd.DataFrame:
     return pd.DataFrame.from_dict(json.loads(request.data)["inputs"])
 
 
+def __encode_input(data: pd.DataFrame) -> pd.DataFrame:
+    data = data.astype(
+        {
+            "Area": "float64",
+            "Build year": "int64",
+            "Floor": "int64",
+            "No. of floors": "int64",
+            "Renovation year": "int64",
+            "price": "float64",
+            "Number of rooms": "int64",
+        }
+    )
+
+    # Categorical data to be encoded
+    encoded_data = data[
+        [
+            "Area",
+            "Build year",
+            "Floor",
+            "No. of floors",
+            "Number of rooms",
+            "Renovation year",
+        ]
+    ]
+
+    # Encoding categorical data with Scikit-Learn LabelEncoder
+    le = preprocessing.LabelEncoder()
+    categorical_values = data[
+        [
+            "Building type",
+            "Equipment",
+            "Heating system",
+            "city",
+            "region",
+            "street",
+        ]
+    ].apply(le.fit_transform)
+
+    # Joining and returning encoded numerical and categorical data
+    return encoded_data.join(categorical_values)
+
+
 @app.route("/predict", methods=["POST"])
 def predict() -> list:
     """
-    Takes data about a house and makes a price prediction with a pretrained model.
+    Takes encoded data about a house and makes a price prediction with a pretrained model.
     Can accept as many house inputs as provided
     """
     input_params = __process_input(request.data)
@@ -32,11 +77,31 @@ def predict() -> list:
     except Exception as error:
         return json.dumps({"error": str(error)}), 400
 
-    #
+    # Add prediction to database
     prediction_data = input_params
-    prediction_data['predicted price'] = predictions
-    db.push_dataframe_to_db('predictions',prediction_data)
-    #
+    prediction_data["predicted price"] = predictions
+    db.push_dataframe_to_db("predictions", prediction_data)
+
+    return json.dumps({"predicted_prices": predictions.tolist()})
+
+
+@app.route("/predict_raw", methods=["POST"])
+def predict_raw() -> list:
+    """
+    Takes encoded data about a house and makes a price prediction with a pretrained model.
+    Can accept as many house inputs as provided
+    """
+    input_params = __encode_input(__process_input(request.data))
+    try:
+        predictions = classifier.predict(input_params)
+    except Exception as error:
+        return json.dumps({"error": str(error)}), 400
+
+    # Add prediction to database
+    prediction_data = input_params
+    prediction_data["predicted price"] = predictions
+    db.push_dataframe_to_db("predictions", prediction_data)
+
     return json.dumps({"predicted_prices": predictions.tolist()})
 
 
